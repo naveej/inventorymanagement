e8e482bc9389e0d17acb3e9a6c1dbd0009024a0d
techtronixx <foxstot@gmail.com>
2024-08-08 21:49:10 +0530

    added login & signup page

--- src/app/SignUp/page.tsx
+++ src/app/SignUp/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/dist/client/link";
 
-const Login = () => {
+const SignUp = () => {
   return (
     <section className="bg-gray-900">
       <div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
@@ -15,8 +15,23 @@ const Login = () => {
         >
           <div className="bg-gray-800 px-8 py-10">
             <div className="mb-6">
-              <h3 className="text-3xl font-bold text-gray-200">Login</h3>
-              <p className="text-sm text-gray-500">Access your account</p>
+              <h3 className="text-3xl font-bold text-gray-200">Sign Up</h3>
+              <p className="text-sm text-gray-500">Create an account</p>
+            </div>
+            <div className="mb-5">
+              <label
+                htmlFor="username"
+                className="block mb-2 text-sm font-medium text-white"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                placeholder="Enter your username"
+                className="w-full px-4 py-2 text-gray-700 bg-gray-200 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
             </div>
             <div className="mb-5">
               <label
@@ -33,7 +48,7 @@ const Login = () => {
                 className="w-full px-4 py-2 text-gray-700 bg-gray-200 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
-            <div className="mb-6">
+            <div className="mb-5">
               <label
                 htmlFor="password"
                 className="block mb-2 text-sm font-medium text-white"
@@ -48,21 +63,36 @@ const Login = () => {
                 className="w-full px-4 py-2 text-gray-700 bg-gray-200 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
+            <div className="mb-6">
+              <label
+                htmlFor="confirm-password"
+                className="block mb-2 text-sm font-medium text-white"
+              >
+                Confirm Password
+              </label>
+              <input
+                type="password"
+                name="confirm-password"
+                id="confirm-password"
+                placeholder="Confirm your password"
+                className="w-full px-4 py-2 text-gray-700 bg-gray-200 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
             <div className="flex justify-center mb-6">
               <button
                 type="submit"
                 className="w-full px-4 py-2 font-bold text-white bg-blue-500 rounded focus:outline-none focus:ring-2 focus:ring-blue-400"
               >
-                Login
+                Sign Up
               </button>
             </div>
             <p className="text-sm text-center text-gray-400">
-              Dont have an account?{" "}
+              Already have an account?{" "}
               <Link
-                href="/SignUp"
+                href="/SignIn"
                 className="text-blue-500 font-bold hover:underline"
               >
-                Sign Up
+                Login
               </Link>
             </p>
           </div>
@@ -72,4 +102,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;

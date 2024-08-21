fd511de0bc782b73a039e663454f1b6f68b19b7c
V Jeevan Kumar <naveej62@gmail.com>
2024-08-21 11:38:29 +0530

    refactor: Update UserProfile page to handle profile updates and form validation

--- src/app/forms/UpdateProfile/page.tsx
+++ src/app/forms/UpdateProfile/page.tsx
@@ -3,45 +3,50 @@ import UserManagement from "@/components/customUI/UserManagement";
 import { Button } from "@/components/ui/button";
 import { motion } from "framer-motion";
 import { useState } from "react";
-import {signIn, useSession} from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import axios from "axios";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
- export const UserProfile = ()=>{
-    const {data: session, update} = useSession();
-    const router = useRouter();
-    
-    const [formData, setFormData] = useState({
+
+export const UserProfile = () => {
+  const { data: session, update } = useSession();
+  const router = useRouter();
+
+  const [formData, setFormData] = useState({
     firstName: session?.user?.firstName as string,
     lastName: session?.user?.lastName as string,
     email: session?.user?.email as string,
     role: session?.user?.role as string,
-    departmentName: session?.user?.departmentName as string
-});
+    departmentName: session?.user?.departmentName as string,
+  });
 
-    const handleUpdate = async (e:any) =>{
-        e.preventDefault();
-        if(!formData.firstName || !formData.lastName || !formData.email){
-            toast.error("Please fill all the fields");
-            return;
-        }
-        try {
-          const response = axios.put(`/api/post/update/userProfile`, { ...formData, _id: session?.user?.id });
-          toast.promise(response, {
-            loading: "Loading...",
-            success: async()=>{
-              return "Profile updated successfully";
-            },
-            error: "Failed to update profile"
-          });
-          const result = await response;
-          router.push('/');
-        } catch (error) {
-          console.error('Error updating profile:', error);
-          toast.error("Failed to update profile");
-        }
-        update();
-        }
+  const handleUpdate = async (e: any) => {
+    e.preventDefault();
+    if (!formData.firstName || !formData.lastName || !formData.email) {
+      toast.error("Please fill all the fields");
+      return;
+    }
+    try {
+      const response = axios.put(`/api/post/update/userProfile`, {
+        ...formData,
+        _id: session?.user?.id,
+      });
+      toast.promise(response, {
+        loading: "Loading...",
+        success: async () => {
+          return "Profile updated successfully";
+        },
+        error: "Failed to update profile",
+      });
+      const result = await response;
+      router.push("/");
+    } catch (error) {
+      console.error("Error updating profile:", error);
+      toast.error("Failed to update profile");
+    }
+    update();
+  };
+
   const handleInputChange = (e: any) => {
     const { name, value } = e.target;
     setFormData((prev) => ({
@@ -49,16 +54,15 @@ import { useRouter } from "next/navigation";
       [name]: value,
     }));
   };
-    
-    return(
-        <>
-        <motion.div
+
+  return (
+    <>
+      <motion.div
         className="p-6 max-w-lg mx-auto mt-12 bg-white dark:bg-gray-900 shadow-lg rounded-md"
         initial={{ opacity: 0, y: 20, filter: "blur(8px)" }}
         animate={{ opacity: 1, y: 0, filter: "blur(0px)" }}
         transition={{
           duration: 0.8,
-
           ease: "easeInOut",
         }}
       >
@@ -114,7 +118,7 @@ import { useRouter } from "next/navigation";
             type="email"
             name="email"
             id="email"
-            disabled
+            disabled={formData.role === "department"}
             value={formData.email}
             onChange={handleInputChange}
             className="mt-1 block w-full px-4 py-2 bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md text-gray-800 dark:text-gray-300"
@@ -132,13 +136,13 @@ import { useRouter } from "next/navigation";
           <input
             name="role"
             id="role"
+            disabled={formData.role === "department"}
             value={formData.role}
-            disabled
             className="mt-1 block w-full px-4 py-2 bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md text-gray-800 dark:text-gray-300"
           />
         </div>
 
-        {/* Department Name*/}
+        {/* Department Name */}
         {formData.role === "department" && (
           <div className="mb-4">
             <label
@@ -154,16 +158,20 @@ import { useRouter } from "next/navigation";
               disabled
               className="mt-1 block w-full px-4 py-2 bg-gray-100 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md text-gray-800 dark:text-gray-300"
             />
-
           </div>
         )}
         <div className="flex mx-auto justify-center">
-          <Button onClick={handleUpdate} className="rounded" variant="secondary">
+          <Button
+            onClick={handleUpdate}
+            className="rounded"
+            variant="secondary"
+          >
             Update
           </Button>
         </div>
       </motion.div>
-        </>
-    )
-}
+    </>
+  );
+};
+
 export default UserProfile;

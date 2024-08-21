70777535e4ff2721d8cceb788309b07d8ae49a6a
V Jeevan Kumar <naveej62@gmail.com>
2024-08-21 10:36:24 +0530

    refactor: Update DashboardCards component to use flex layout for stats display

--- src/components/DashBoardCards.tsx
+++ src/components/DashBoardCards.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
+import { Users } from "lucide-react";
 
 const DashboardCards = () => {
   const [dataCounts, setDataCounts] = useState({
@@ -54,21 +55,9 @@ const DashboardCards = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="stats shadow flex justify-between w-full max-w-4xl p-4 items-center">
-        <div className="stat">
+        <div className="stat flex flex-col items-center justify-center">
           <div className="stat-figure text-secondary">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block h-8 w-8 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              ></path>
-            </svg>
+            <Users size={32} className="text-black dark:text-white" />
           </div>
           <div className="stat-title">Asset Maintenance</div>
           <div className="stat-value">
@@ -76,21 +65,9 @@ const DashboardCards = () => {
           </div>
         </div>
 
-        <div className="stat">
+        <div className="stat flex flex-col items-center justify-center">
           <div className="stat-figure text-secondary">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block h-8 w-8 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
-              ></path>
-            </svg>
+            <Users size={32} className="text-black dark:text-white" />
           </div>
           <div className="stat-title">Documented Information</div>
           <div className="stat-value">
@@ -98,21 +75,9 @@ const DashboardCards = () => {
           </div>
         </div>
 
-        <div className="stat">
+        <div className="stat flex flex-col items-center justify-center">
           <div className="stat-figure text-secondary">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block h-8 w-8 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"
-              ></path>
-            </svg>
+            <Users size={32} className="text-black dark:text-white" />
           </div>
           <div className="stat-title">Calibration Schedule</div>
           <div className="stat-value">
@@ -120,21 +85,9 @@ const DashboardCards = () => {
           </div>
         </div>
 
-        <div className="stat">
+        <div className="stat flex flex-col items-center justify-center">
           <div className="stat-figure text-secondary">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block h-8 w-8 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              ></path>
-            </svg>
+            <Users size={32} className="text-black dark:text-white" />
           </div>
           <div className="stat-title">NC Output</div>
           <div className="stat-value">{dataCounts.ncOutput} entries</div>
